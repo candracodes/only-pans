e6ebdd32703792acd61952159cf3c41fd2ccdec3
Thu Phan <alicebe.phan@gmail.com>
2021-11-09 00:22:30 -0600

    Created server.js file, updated the folder structure.

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,12 @@
 // THIS IS A PLACEHOLDER
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
