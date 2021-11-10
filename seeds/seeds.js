aaffc5c82c25af0d55ff5d7d126825644ea5b1b2
Austin Pratt <hargis32@gmail.com>
2021-11-10 16:24:13 -0600

    added seed function, added first recipe seed data

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,1 +1,18 @@
-// THIS IS A PLACEHOLDER
+const sequelize = require('../config/connection');
+const { User, Recipe } = require('../models');
+
+const recipeData = require('./recipeData.json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await Recipe.bulkCreate(recipeData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
