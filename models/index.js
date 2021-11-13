8e4ebf45bb2a353f3f9952220f8045184079b1ff
Thu Phan <alicebe.phan@gmail.com>
2021-11-12 19:01:46 -0600

    I modified the index.js file in models folder by setting up table associations for user, recipe and category

--- models/index.js
+++ models/index.js
@@ -1,1 +1,26 @@
-// THIS IS A PLACEHOLDER
+const User = require('./User');
+const Category = require('./Category');
+const Recipe = require('./Recipe');
+
+// Associations between recipe and category
+Category.hasMany(Recipe, {
+    foreignKey: 'category_id',
+});
+
+Recipe.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+// Associations between user and recipe
+
+User.hasMany(Recipe, {
+    foreignKey: 'user_id',
+
+});
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id',
+
+})
+module.exports = { User, Category, Recipe };
+
+
