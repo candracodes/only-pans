6abb3098df631d30abcf4c8c2403d0c7413d31fc
Thu Phan <alicebe.phan@gmail.com>
2021-11-14 17:00:47 -0600

    Fixed bugs in login.handlebars and conflicts in userRoutes, created homepage route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+// routes begin with api/users
 // Create new user
 router.post('/', async (req, res) => {
     try {
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 });
   
 // Log in
-router.post('login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const dbUserData = await User.findOne({
             where: {
--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,15 +1,7 @@
+// GET ROUTES that render homepage.handlebars
 const router = require('express').Router();
-const { route } = require('.');
-const { User } = require('../models');
-// Import the custom middleware
 const withAuth = require('../utils/auth');
 
-// this code was written on Wednesday ( we want to display login page before homeage.)
-router.get('/', async (req,res) => {
-    res.render('login');
-});
-
-// Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async(req, res) => {
     try {
         const dbUserData = await User.findAll({
@@ -27,16 +19,5 @@ router.get('/', withAuth, async(req, res) => {
     }
 
 });
-
-
-router.get('/login', (req, res) => {
-    // If a session exists, redirect the request to te homepage
-    if (res.session.logged_in) {
-        res.redirect('/'); 
-        return;
-    }
-    res.render('login');
-});
-
 
 module.exports = router;
