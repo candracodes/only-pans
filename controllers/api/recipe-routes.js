0157e913f68c2f42fbe1977f8efb658173b281a6
Candra Fried <candracodes@gmail.com>
2021-11-15 19:39:37 -0600

    RECIPES PAGE SUCCESSFULLY RENDERS

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -1,37 +1,28 @@
-// TODO: AT PRESENT, RECIPES STILL DOESN'T RENDER. JUST GETTING THE PATH STARTED SO WE CAN REVIEW AS A  GROUP
 const router = require('express').Router();
-const { Recipe } = require('../../models');
+const express = require('express');
+const { Recipe, Category } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// router.post('/', async (req, res) => {
-//     try {
-//       const newRecipe = await Recipe.create({
-//         ...req.body
-//         // user_id: req.session.user_id,
-//       });
-  
-//       res.status(200).json(newRecipe);
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
-
-  router.get('/', withAuth, async(req, res) => {
-    try {
-        const dbUserData = await User.findAll({
-            attributes: { exclude: ['password'] },
-            order: [['email', 'ASC']],
-        });
-
-        const users = dbUserData.map((project) => project.get({ plain: true }));
-        res.render('recipes', {
-            users,
-            logged_in: req.session.logged_in,
-        });
-    }   catch (err) {
-        res.status(500).json(err);
-    }
-
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbRecipeData = await Recipe.findAll({
+      include: [
+        {
+          model: Category
+        },
+      ],
+    });
+    const recipes = dbRecipeData.map((recipes) =>
+    recipes.get({ plain: true })
+    );
+    res.render('recipes', {
+        recipes,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
