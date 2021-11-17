68a0f9c14902dc1f6b237fc8e7e1f2895c2c7cb7
Candra Fried <candracodes@gmail.com>
2021-11-16 18:54:39 -0600

    Successfully rendered starters route that filters recipes for solely starters

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const homeRoutes = require('./homeRoutes.js');
 const homepage = require('./homepage.js');
 const userRoutes = require('./user-routes');
 const recipeRoutes = require('./recipe-routes');
+const starterRoutes = require('./starter-routes');
 
 router.use('/', homeRoutes);
 router.use('/homepage', homepage);
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
+router.use('/starters', starterRoutes);
 
 module.exports = router;
