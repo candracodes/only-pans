86947288b901cc42cff4e902d8b4b4e3ab81d689
Candra Fried <candracodes@gmail.com>
2021-11-16 19:24:25 -0600

    Successfully rendered sides, entrees, starters, and desserts

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,17 @@ const homepage = require('./homepage.js');
 const userRoutes = require('./user-routes');
 const recipeRoutes = require('./recipe-routes');
 const starterRoutes = require('./starter-routes');
+const sidesRoutes = require('./side-routes');
+const dessertRoutes = require('./dessert-routes');
+const entreeRoutes = require('./entree-routes');
 
 router.use('/', homeRoutes);
 router.use('/homepage', homepage);
 router.use('/users', userRoutes);
 router.use('/recipes', recipeRoutes);
 router.use('/starters', starterRoutes);
+router.use('/sides', sidesRoutes);
+router.use('/desserts', dessertRoutes);
+router.use('/entrees', entreeRoutes);
 
 module.exports = router;
---
+++ controllers/side-routes.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const express = require('express');
+const { Recipe, Category } = require('../models');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbRecipeData = await Recipe.findAll({
+      // should I include something here that says where {recipe_type: req.body.starters}
+      where: {
+        recipe_type: "sides"
+    },
+      
+      include: [
+        {
+          model: Category
+        },
+      ],
+    });
+    
+    const recipes = dbRecipeData.map((recipes) =>
+    recipes.get({ plain: true })
+    );
+    console.log(recipes);
+    res.render('sides', {
+        recipes,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// THIS ROUTE WAS CREATED SOLELY TO POPULATE MODALS
+router.get('/filter', withAuth, async (req, res) => {
+  try {
+    const dbRecipeData = await Recipe.findAll({
+      include: [
+        {
+          model: Category
+        },
+      ],
+    });
+    
+    const recipes = dbRecipeData.map((recipes) =>
+    recipes.get({ plain: true })
+    );
+    console.log(recipes);
+    res.json( recipes )
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
+module.exports = router;
